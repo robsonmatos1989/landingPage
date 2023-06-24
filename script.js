dadd290ae329594ba57154b99821da830e0397c0
Robson Matos <nosbor_album@hotmail.com>
2023-06-24 15:04:32 -0300

    Aplicando funções de validação

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function numero(){
-	numero = document.getElementById("telefone").value;
-	document.getElementById("resultado").innerHTML = "O número é: " +numero;
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+
+const spans = document.querySelectorAll('.span-required');
+
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+function nameValidate(){
+	if(campos[0].value.length < 3){
+		console.log("Erro")
+	}
+	else{
+		console.log("correto");
+	}
 }
