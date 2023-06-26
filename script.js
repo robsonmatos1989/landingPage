a7ea69626f6e1eb4309aeb0845ffb1b01a11258e
Robson Matos <nosbor_album@hotmail.com>
2023-06-26 17:15:58 -0300

    Aplicado validação para o input (telefone).

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const spans = document.querySelectorAll('.span-required');
 
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const telRegex = /^(1[1-9]|[4689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
+
 
 function setError(index){
 	campos[index].style.border = '2px solid #e63636';
@@ -35,4 +37,17 @@ function emailValidate(){
 	else{
 		setError(1); //Enquanto não atende ao teste, é setado o erro
 	}
+}
+
+function telValidate(){
+	if(telRegex.test(campos[2].value)){ /**/
+		
+	removeError(2);//Aqui inverto a ordem do setError e do removeError, pois só é removido
+	//após atender o teste que é feito com o emailRegex 
+
+	}
+	else{
+		setError(2); //Enquanto não atende ao teste, é setado o erro
+	}
+
 }
